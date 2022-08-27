c56670db83eee29f937f302b2d59284674b615b2
Gökmen Görgen <gkmngrgn@gmail.com>
2022-08-27 19:35:29 +0300

    fix python paths.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
-OS_NAME=$(python -c 'import platform; print(platform.machine().lower())')
-ARCH_TYPE=$(python -c 'import platform; print(platform.processor().lower())')
+export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+
+OS_NAME=$(python -c 'import platform; print(platform.system().lower())')
+ARCH_TYPE=$(python -c 'import platform; print(platform.machine().lower())')
 PY_VERSION=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
 DIR_NAME="dosh-${OS_NAME}-${ARCH_TYPE}-py${PY_VERSION}"
 

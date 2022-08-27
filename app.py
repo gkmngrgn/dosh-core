e5b56400b136a1816e4bf2559b12cb8bd6cd905d
Gökmen Görgen <gkmngrgn@gmail.com>
2022-08-27 22:09:47 +0300

    CircleCI Configuration (#10)
    * started using pyinstaller instead of pyoxidizer.
    
    * build on macos - linux - windows with CircleCI.

---
+++ app.py
@@ -0,0 +1,6 @@
+"""DOSH runner."""
+
+from dosh import cli
+
+if __name__ == "__main__":
+    cli.run()
---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+
+OS_NAME=$(python -c 'import platform; print(platform.system().lower())')
+ARCH_TYPE=$(python -c 'import platform; print(platform.machine().lower())')
+PY_VERSION=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
+DIR_NAME="dosh-${OS_NAME}-${ARCH_TYPE}-py${PY_VERSION}"
+
+pip install poetry
+poetry install
+poetry run poe build
+
+cd ./dist/
+mv dosh $DIR_NAME
+tar -czvf $DIR_NAME.tar.gz $DIR_NAME/*

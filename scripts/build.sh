0b6d7ba8da1c2b4feb92e94a91430d8a738cbb7d
Gökmen Görgen <gkmngrgn@gmail.com>
2022-08-27 17:47:37 +0300

    test build script.

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+OS_NAME=$(python -c 'import platform; print(platform.system().lower())')
+ARCH_TYPE=$(python -c 'import platform; print(platform.processor().lower())')
+PY_VERSION=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
+DIR_NAME=dosh-${OS_NAME}-${ARCH_TYPE}-py${PY_VERSION}
+
+pip install poetry
+poetry install
+poetry run poe build
+
+cd ./dist/
+mv dosh $DIR_NAME
+tar -czvf $DIR_NAME.tar.gz $DIR_NAME/*

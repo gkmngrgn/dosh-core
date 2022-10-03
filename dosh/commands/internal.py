ed34e2c13d1660bd349a6fe85f8d30580023db3b
Gökmen Görgen <gkmngrgn@gmail.com>
2022-10-03 21:30:26 +0200

    update init file.

--- dosh/commands/internal.py
+++ dosh/commands/internal.py
@@ -15,28 +15,21 @@ def init_config(config_path: Path) -> CommandResult[None]:
         )
 
     content = textwrap.dedent(
-        """\
-        -- task: say hello to anyone. it takes an argument.
-        cmd.add_task{
-           name="say_hello",
-           description="say hello to anyone",
-           command=function(there)
-              there = there or "world"
-              cmd.info("hello " .. there .. "!")
-           end
-        }
+        r"""local name = "there"                         -- you can use all features of Lua programming language.
 
-        -- task: install my favourite apps.
-        cmd.add_task{
-           name="check_my_apps",
-           command=function()
-              if not env.IS_ZSH then
-                 cmd.error("did you forget to install or activate zsh?")
-              else
-                 cmd.info("you have zsh.")
-              end
-           end
-        }
+            local function hello(there)                  -- even you can define your custom functions.
+                there = there or name
+                local message = "Hello, " .. there .. "!"
+                cmd.run("osascript -e 'display notification \"" .. message .. "\" with title \"Hi!\"'")
+            end
+
+            cmd.add_task{                                -- cmd comes from dosh.
+               name="hello",                             -- task name, or subcommand for your cli.
+               description="say hello",                  -- task description for the help output.
+               required_commands={"osascript"},          -- check if the programs exist before running the task.
+               environments={"development", "staging"},  -- DOSH_ENV variable should be either development or staging to run this task.
+               command=hello                             -- run hello function with its parameters when the task ran.
+            }
         """
     )
 

c1b928c4db66cc04ddae2401a5fce07b07dfb729
Gökmen Görgen <gkmngrgn@gmail.com>
2022-12-12 02:32:03 +0300

    fix build issue.

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,7 +5,6 @@ ARCH_TYPE=$(python -c 'import platform; print(platform.machine().lower())')
 PY_VERSION=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
 DIR_NAME="dosh-${OS_NAME}-${ARCH_TYPE}-py${PY_VERSION}"
 
-poetry install --with=dev
 poetry run pyinstaller app.py --name=dosh --console --noconfirm
 
 cd ./dist/

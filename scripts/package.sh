c7cda78c0bf9989eeb0184e5757b0331f8857971
Gökmen Görgen <gkmngrgn@gmail.com>
2022-12-12 10:13:09 +0100

    Fix build issue. (#36)
    * fix build issue.
    
    * test installing poetry with pip.

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,10 +3,20 @@ export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
 OS_NAME=$(python -c 'import platform; print(platform.system().lower())')
 ARCH_TYPE=$(python -c 'import platform; print(platform.machine().lower())')
 PY_VERSION=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
+POETRY_VERSION="1.3.0"
 DIR_NAME="dosh-${OS_NAME}-${ARCH_TYPE}-py${PY_VERSION}"
 
-poetry install --with=dev
-poetry run pyinstaller app.py --name=dosh --console --noconfirm
+echo "Operating System  : $OS_NAME"
+echo "Architecture Type : $ARCH_TYPE"
+echo "Python Version    : $PY_VERSION"
+echo "Poetry Version    : $POETRY_VERSION"
+
+export POETRY_HOME=~/poetry
+python -m venv $POETRY_HOME
+
+$POETRY_HOME/bin/pip install "poetry==$POETRY_VERSION"
+$POETRY_HOME/bin/poetry install --with=dev
+$POETRY_HOME/bin/poetry run pyinstaller app.py --name=dosh --console --noconfirm
 
 cd ./dist/
 mv dosh $DIR_NAME

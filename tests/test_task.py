d4179ca80c585a2d03c7fcf9dc5ce991d60dd99d
Gökmen Görgen <gkmngrgn@gmail.com>
2022-12-30 00:44:22 +0300

    fix platform checking.

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,27 +1,27 @@
+import platform
+
 from dosh.commands.base import CommandResult, CommandStatus, OperatingSystem, Task
 from dosh.config import ConfigParser
 
 
 def test_get_current_operating_system(monkeypatch):
-    monkeypatch.setenv("OSTYPE", "darwin-blablabla")
+    monkeypatch.setattr(platform, "system", lambda: "Darwin")
     assert OperatingSystem.get_current() == OperatingSystem.MACOS
 
-    monkeypatch.setenv("OSTYPE", "linux")
+    monkeypatch.setattr(platform, "system", lambda: "Linux")
     assert OperatingSystem.get_current() == OperatingSystem.LINUX
 
-    monkeypatch.setenv("OSTYPE", "msys")
+    monkeypatch.setattr(platform, "system", lambda: "Windows")
     assert OperatingSystem.get_current() == OperatingSystem.WINDOWS
 
-    monkeypatch.setenv("OSTYPE", "bsd")
+    monkeypatch.setattr(platform, "system", lambda: "FreeBSD")
     assert OperatingSystem.get_current() == OperatingSystem.UNSUPPORTED
 
 
-def test_task_if_available_on_linux(monkeypatch, caplog):
+def test_task_if_available_on_linux(monkeypatch, caplog, capsys):
     def print_hello():
         print("hello")
         return CommandResult(CommandStatus.OK)
-
-    monkeypatch.setenv("OSTYPE", "linux")
 
     task_hello = Task(
         name="hello",
@@ -30,11 +30,21 @@ def test_task_if_available_on_linux(monkeypatch, caplog):
     )
     config_parser = ConfigParser(content="")
     config_parser.tasks = [task_hello]
+
+    # run task on unsupported operating system first.
+    monkeypatch.setattr(platform, "system", lambda: "Linux")
     config_parser.run_task("hello", params=[])
 
     assert len(caplog.records) == 1
-
-    log_record = caplog.records[0]
+    assert capsys.readouterr().out == ""
     assert (
-        log_record.message == "The task `hello` does not work on this operating system."
+        caplog.records[0].message
+        == "The task `hello` does not work on this operating system."
     )
+
+    # run task on macos. that should be worked.
+    monkeypatch.setattr(platform, "system", lambda: "Darwin")
+    config_parser.run_task("hello", params=[])
+
+    assert len(caplog.records) == 1
+    assert capsys.readouterr().out == "hello\n"
